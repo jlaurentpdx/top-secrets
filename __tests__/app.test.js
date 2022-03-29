7438bb3f9948f6991cd7356e15d5b7b368e33d0c
Jordan Laurent <jlaurent.pdx@gmail.com>
2022-03-29 14:28:58 -0700

    user post/signin initial test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -21,5 +22,14 @@ describe('top-secrets routes', () => {
       id: expect.any(String),
       email: 'jojo@defense.gov',
     });
+  });
+
+  it('signs in an existing user on POST to /sessions', async () => {
+    const user = { email: 'jojo@defense.gov', password: 'codobyjojo' };
+
+    const newUser = await UserService.create(user);
+    const res = await request(app).post('/api/v1/users/sessions').send(newUser);
+
+    expect(res.body).toEqual({ message: 'Signed in successfully', newUser });
   });
 });
